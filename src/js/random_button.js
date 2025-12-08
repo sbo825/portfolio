8076b1395ace25e001af887df342f264e4a850e6
root <root@113-Portfolio.mercusysddns.com>
2025-12-08 21:28:38 +0000

    New pages, subpages and content

--- src/js/random_button.js
+++ src/js/random_button.js
@@ -1,9 +1,12 @@
 // script.js
-    function irASubpaginaAleatoria() {
+    function random() {
     const paginas = [
         "./src/pages/linux.html",
         "./src/pages/linux-subpages/apache2.html",
-        ""
+        "./src/pages/ciberseguridad.html",
+        "./src/pages/ciberseguridad-subpages/burpsuite.html",
+        "./src/pages/ansible.html",
+        "./src/pages/ansible-subpages/lamp.html"
 ];
 
     const indice = Math.floor(Math.random() * paginas.length);
